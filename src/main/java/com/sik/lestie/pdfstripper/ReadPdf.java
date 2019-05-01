d36d86d213958f36b339721ce6c8a3b1897da6c9
Si Kusners <simon.kusners@gmail.com>
2019-05-01 20:32:29 +0100

    basic worky thing

--- src/main/java/com/sik/lestie/pdfstripper/ReadPdf.java
+++ src/main/java/com/sik/lestie/pdfstripper/ReadPdf.java
@@ -1,39 +1,69 @@
 package com.sik.lestie.pdfstripper;
 
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.text.PDFTextStripper;
-import org.apache.pdfbox.text.PDFTextStripperByArea;
-
 import java.io.File;
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.Set;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
 
 public class ReadPdf {
 
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 
-        try (PDDocument document = PDDocument.load(new File("/path-to/abc.pdf"))) {
+		// Security.addProvider( new
+		// org.bouncycastle.jce.provider.BouncyCastleProvider() );
 
-            document.getClass();
+		// String fileMask = "*.pdf";
+		File folder = new File("/Volumes/MacData/sik/Documents/");
+		if (!folder.isDirectory()) {
+			throw new IllegalArgumentException("Not a folder: " + folder);
+		}
 
-            if (!document.isEncrypted()) {
-			
-                PDFTextStripperByArea stripper = new PDFTextStripperByArea();
-                stripper.setSortByPosition(true);
+		for (File pdf : folder.listFiles()) {
 
-                PDFTextStripper tStripper = new PDFTextStripper();
+			if (pdf.getName().endsWith(".pdf")) {
 
-                String pdfFileInText = tStripper.getText(document);
-                //System.out.println("Text:" + st);
+				try {
 
-				// split by whitespace
-                String lines[] = pdfFileInText.split("\\r?\\n");
-                for (String line : lines) {
-                    System.out.println(line);
-                }
+					PDDocument document = PDDocument.load(new File(pdf.getAbsolutePath()));
 
-            }
+					document.getClass();
 
-        }
+					if (!document.isEncrypted()) {
 
-    }
+						Set<String> keys = document.getDocumentInformation().getMetadataKeys();
+
+						System.out.println(pdf.getName() + " - " + document.getDocumentInformation().getTitle() + ", "
+								+ document.getDocumentInformation().getAuthor() + ", "
+								+ document.getDocumentInformation().getProducer() + ", "
+								+ document.getDocumentInformation().getCreator() + ", "
+								+ document.getDocumentInformation().getCreationDate().get(Calendar.YEAR));
+
+						// PDFTextStripperByArea stripper = new PDFTextStripperByArea();
+						// stripper.setSortByPosition(true);
+
+						// PDFTextStripper tStripper = new PDFTextStripper();
+
+						// String pdfFileInText = tStripper.getText(document);
+						// tStripper.
+						// System.out.println("Text:" + st);
+
+						// split by whitespace
+						// String lines[] = pdfFileInText.split("\\r?\\n");
+						// for (String line : lines) {
+						// System.out.println(line);
+						// }
+
+					}
+					document.close();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					System.err.println("Meh!");
+				}
+
+			}
+		}
+
+	}
 }
