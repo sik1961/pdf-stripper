0488ac02456f217a127be38e229c1aaafc8899cf
Si Kusners <simon.kusners@gmail.com>
2019-05-01 18:38:31 +0100

    initial commit

---
+++ src/main/java/com/sik/lestie/pdfstripper/ReadPdf.java
@@ -0,0 +1,39 @@
+package com.sik.lestie.pdfstripper;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.text.PDFTextStripperByArea;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ReadPdf {
+
+    public static void main(String[] args) throws IOException {
+
+        try (PDDocument document = PDDocument.load(new File("/path-to/abc.pdf"))) {
+
+            document.getClass();
+
+            if (!document.isEncrypted()) {
+			
+                PDFTextStripperByArea stripper = new PDFTextStripperByArea();
+                stripper.setSortByPosition(true);
+
+                PDFTextStripper tStripper = new PDFTextStripper();
+
+                String pdfFileInText = tStripper.getText(document);
+                //System.out.println("Text:" + st);
+
+				// split by whitespace
+                String lines[] = pdfFileInText.split("\\r?\\n");
+                for (String line : lines) {
+                    System.out.println(line);
+                }
+
+            }
+
+        }
+
+    }
+}
